ade8be242f056fb2023dcdfa9d8e304c4b434e4c
Yang Cui <ycui55@student.monash.edu>
2018-05-13 14:22:58 +1000

    updated the adopt class
    added comment

--- FIT2099_Assignment_Base_2018/src/starwars/actions/Adopt.java
+++ FIT2099_Assignment_Base_2018/src/starwars/actions/Adopt.java
@@ -1,31 +1,46 @@
 package starwars.actions;
 
 import edu.monash.fit2099.simulator.userInterface.MessageRenderer;
-import starwars.SWAction;
 import starwars.SWActor;
 import starwars.SWAffordance;
 import starwars.SWEntityInterface;
 import starwars.entities.actors.Droid;
 import starwars.entities.actors.Player;
-
+/**
+ * 
+ * @author CUIYANG and kapo Ho
+ * Command to allow a player adopt a </code>Droid</code> when they meet.
+ * Modified date:13/05/2018
+ */
 public class Adopt extends SWAffordance {
-
+	/**
+	 * This is the constructor for the </code>Adopt</code> affordance .
+	 * @param theTarget instance of </code>SWEntityInterface</code> used to initialize the </code>Adopt</code> affordance
+	 * @param m instance of </code>MessageRenderer</code> used to initialize the </code>Adopt</code> affordance
+	 */
 	public Adopt(SWEntityInterface theTarget, MessageRenderer m) {
 		super(theTarget, m);
 		// TODO Auto-generated constructor stub
 		
 	}
-
+/**
+ * @param: instance of </code>SWActor</code>
+ * @return: true if a SWActor could adopt a </code>Droid</code> false otherwise.
+ */
 	@Override
 	public boolean canDo(SWActor a) {
 		// TODO Auto-generated method stub
 		return a instanceof Player;
 	}
-
+/**
+ * Perform the </code>Adopt</code> action by setting the owner of the </code>Droid</code> to that </code>SWActor</code> 
+ * and reomove the </code>Adopt</code> affordance.
+ * @param:instance of </code>SWActor</code>
+ * 
+ * 
+ */
 	@Override
 	public void act(SWActor a) {
-
-
 		// TODO Auto-generated method stub
 		if (target instanceof Droid){
 			if (this.canDo(a)){
@@ -34,10 +49,15 @@ public class Adopt extends SWAffordance {
 					thetarget.setOwner(a);
 					target.removeAffordance(this);
 				}
-				}
 			}
 		}
-		
+	}
+	/**
+	 * A String describing what this action will do, suitable for display in a user interface
+	 * 
+	 * @author kapa Ho and YangCui
+	 * @return String comprising "Adopt" and the short description of the target of this <code>Adopt</code>
+	 */
 
 	@Override
 	public String getDescription() {
