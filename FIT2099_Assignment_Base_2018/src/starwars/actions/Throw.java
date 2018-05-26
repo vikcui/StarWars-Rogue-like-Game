7cec51d6736ca3486bec53d824368a62c8039b5e
kpho9 <kpho9@stduent.monash.edu>
2018-05-26 18:54:23 +1000

    Update comment for the Throw class
    Update three constants attributes for the throw class.

--- FIT2099_Assignment_Base_2018/src/starwars/actions/Throw.java
+++ FIT2099_Assignment_Base_2018/src/starwars/actions/Throw.java
@@ -15,25 +15,63 @@ import starwars.SWLocation;
 import starwars.entities.Grenade;
 
 
+/**
+ * A new class Throw in the package starwars.entities.actors.behaviors.
+ * Throw is a action of a actor, and in star war scenario, the throw that only can be performed by actor when item carried by actor is grenade.
+ * The class has its own constructor and three more other functions : canDo(), act(), getDescription().
+ * There are three constant attributes inside the class: maxExplosion, intermediateExplosion, minExplosion.
+ * @author Kapo Ho and cuiyang
+ *
+ */
 public class Throw extends SWAffordance implements SWActionInterface {
 	
-	
+	// the int attribute representing the maximum explosion damage
 	static final int maxExplosion=20;
+	// the int attribute representing the intermediate explosion damage
 	static final int intermediateExplosion=10;
+	// the int attribute representing the minimum explosion damage
 	static final int minExplosion=5;
 
+	/**
+	 * The constructor for Throw class, will initialize the the message renderer, the target and 
+	 * set the priority of this <code>Action</code> to 1 (lowest priority is 0)
+	 * 
+	 * @param </code>theTarget</code>, a instance of SWEntityInterface used to initialize a new Throw object.
+	 * @param </code>m</code>, a instance of MessageRenderer used to initialize a new Throw object.
+	 */
 	public Throw(SWEntityInterface theTarget, MessageRenderer m) {
 		super(theTarget, m);	
 		priority = 1;
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Return if or not this <code>Throw</code> can be performed by the  <code>SWActor a</code>.
+	 * <p>
+	 * This method returns true if and only if <code>a</code> is carrying a instance of grenade.
+	 * 
+	 * @author 	Ka Po Ho and Yang Cui
+	 * @param a the <code>SWActor</code> being queried
+	 * @return true if the item carried by the actor is a instance of grenade, false otherwise
+	 * @see		{@link starwars.SWActor#getItemCarried()}
+	 */
 	@Override
 	public boolean canDo(SWActor a) {
 		// TODO Auto-generated method stub
 		return a.getItemCarried() instanceof Grenade;
 	}
 
+	/**
+	 * Perform the <code>Throw</code> action by the following steps:
+	 * 1.Check if the item carried by the actor is Grenade or not, if yes goto 2, false do nothing.
+	 * 2.Add all the possible directions into directions arrayList.
+	 * 3.check whether the actor(grenade) and other entities in the world is in the same location, if yes, then set the damage taken by them be 20
+	 * 4.Then, check if there are some entities in the neighbor location of the grenade(i.e.: can be reached in one step from the location where the grenade is thrown),
+	 * if yes, then set the damage taken by them be 10, otherwise do nothing.
+	 * 5.Then, check if there are some entities in the neighbor location of the neighbor location of the grenade(i.e.:can be reached in two steps from the location where the grenade is thrown)
+	 * if yes, then set the damage taken by them be 5, otherwise do nothing.
+	 * At the end, the item carried by the actor should be set to null as the grenade has already been thrown and cause damage.
+	 */
 	@Override
 	public void act(SWActor a) {
 		// TODO Auto-generated method stub
@@ -82,6 +120,12 @@ public class Throw extends SWAffordance implements SWActionInterface {
 		}
 	}
 
+	/**
+	 * A String describing what this action will do, suitable for display in a user interface
+	 * 
+	 * @author Kapo Ho and YangCui
+	 * @return String comprising "Throw" and the short description of the target of this <code>Throw</code>
+	 */
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
